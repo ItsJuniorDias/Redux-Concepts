ca27d5d19596ba689150d7dd2c78d9d5fa65e975
Junior Dias <juniordias_@live.com>
2020-10-22 12:00:48 -0300

    chore: state Create Store

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import Catalog from './components/Catalog';
 
+
+import store from './store';
 
 // import { Container } from './styles';
 
 const App: React.FC = () => {
-  return <h1>Hello World</h1>;
+
+  return (
+    <Provider store={store}>
+      <Catalog />
+    </Provider>
+  );
 }
 
 export default App;
