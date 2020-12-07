a3341a2c00ab0b96cf5f38c5dd9c99d25bb8d7f6
Alexandre Junior <juniordias_@live.com>
2020-12-06 22:14:13 -0300

    chore: Actions

--- concepts-new/src/store/modules/cart/reducer.ts
+++ concepts-new/src/store/modules/cart/reducer.ts
@@ -1,11 +1,12 @@
 import { Reducer } from "redux";
 import { ICartState } from "./types";
 
+const INITIAL_STATE: ICartState = {
+  items: []
+}
 
 const cart:Reducer<ICartState> = () => {
-  return {
-    items: []
-  };
+  return INITIAL_STATE;
 }
 
 export default cart;
