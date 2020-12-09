459ac44445b6b5367a9c0426bee8f681e824f394
Alexandre Junior <juniordias_@live.com>
2020-12-09 17:27:22 -0300

    chore: Actions Types

--- concepts-new/src/store/modules/cart/reducer.ts
+++ concepts-new/src/store/modules/cart/reducer.ts
@@ -1,5 +1,5 @@
 import { Reducer } from "redux";
-import { ICartState } from "./types";
+import { ActionTypes, ICartState } from "./types";
 import produce from 'immer';
 
 const INITIAL_STATE: ICartState = {
@@ -10,7 +10,7 @@ const INITIAL_STATE: ICartState = {
 const cart:Reducer<ICartState> = (state = INITIAL_STATE, action) => {
   return produce(state, draft => {
     switch(action.type) { 
-      case 'ADD_PRODUCT_TO_CART_SUCCESS': {
+      case ActionTypes.addProductToCartSuccess: {
 
       const { product } = action.payload;
        
@@ -40,7 +40,7 @@ const cart:Reducer<ICartState> = (state = INITIAL_STATE, action) => {
       //  }
       }
 
-      case 'ADD_PRODUCT_TO_CART_FAILURE': {
+      case ActionTypes.addProductToCartFailure: {
         draft.failedStockCheck.push(action.payload.productId);
 
         break;
